b6dd20acb596e6944de7a1c8c254a958f8a1376f
lucas <lucass.michels@gmail.com>
2023-10-14 21:03:47 -0400

    adding java code to git

---
+++ src/main/java/com/lucasmichels/todolist/task/TaskModel.java
@@ -0,0 +1,40 @@
+package com.lucasmichels.todolist.task;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity(name = "tb_tasks")
+public class TaskModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+    private String description;
+    
+    @Column(length = 50)
+    private String title;
+    private LocalDateTime startAt;
+    private LocalDateTime endAt;
+    private String priority;
+
+    private UUID idUser;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    public void setTitle(String title) throws Exception {
+        if (title.length() > 50) { //mensagem de erro quando title tem mais q 50 caracteres
+            throw new Exception("Title has a limit of 50 characters");
+        }
+        this.title = title;
+    }
+}
---
+++ src/main/java/com/lucasmichels/todolist/user/UserModel.java
@@ -0,0 +1,31 @@
+package com.lucasmichels.todolist.user;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity(name = "tb_users")
+public class UserModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID Id;
+
+    @Column(unique = true)
+    private String username;
+    private String name;
+    private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+}
+
